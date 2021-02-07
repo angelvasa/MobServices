f7f0d7de9b3fc945eef51b57c03ed85da2b217e5
Angel Vasa <avasa@mobiquityinc.com>
2021-02-07 11:57:24 +0530

    Initial Commit

---
+++ MobServices/MobServices.h
@@ -0,0 +1,18 @@
+//
+//  MobServices.h
+//  MobServices
+//
+//  Created by Angel Vasa on 07/02/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MobServices.
+FOUNDATION_EXPORT double MobServicesVersionNumber;
+
+//! Project version string for MobServices.
+FOUNDATION_EXPORT const unsigned char MobServicesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MobServices/PublicHeader.h>
+
+
