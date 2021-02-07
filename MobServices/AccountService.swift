a9e416ad5ece02f44b042835a582efe72a72dd1f
Angel Vasa <avasa@mobiquityinc.com>
2021-02-07 13:40:40 +0530

    Added Account Services
    Signed-off-by: Angel Vasa <avasa@mobiquityinc.com>

---
+++ MobServices/AccountService.swift
@@ -0,0 +1,14 @@
+//
+//  AccountService.swift
+//  MobServices
+//
+//  Created by Angel Vasa on 07/02/21.
+//
+
+import Foundation
+
+public class AccountService {
+    public init () {
+        print("ACCOUNT SERVICE")
+    }
+}
